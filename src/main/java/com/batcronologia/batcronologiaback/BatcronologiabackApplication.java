3c20112bd374be33276c1a2ba9959343adbe9e94
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2022-02-24 16:44:06 -0300

    Project started

---
+++ src/main/java/com/batcronologia/batcronologiaback/BatcronologiabackApplication.java
@@ -0,0 +1,13 @@
+package com.batcronologia.batcronologiaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatcronologiabackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatcronologiabackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batcronologia/batcronologiaback/BatcronologiabackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batcronologia.batcronologiaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatcronologiabackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
