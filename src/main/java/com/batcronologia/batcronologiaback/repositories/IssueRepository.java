98bab3abfbbd7c855192ae34ec6dc11d7493a1c4
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2022-05-06 18:15:46 -0300

    Fixed issue boot problem

--- src/main/java/com/batcronologia/batcronologiaback/repositories/IssueRepository.java
+++ src/main/java/com/batcronologia/batcronologiaback/repositories/IssueRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IssueRepository extends JpaRepository {
+public interface IssueRepository extends JpaRepository<Issue, Long> {
     Page<Issue> findAll(Pageable pageable);
 }
